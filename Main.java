01f97227009c24acc9662904d531833a759ceb71
Maddox Scott <maddoxscott99@gmail.com>
2022-10-20 13:58:19 -0700

    Large maze added.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Maze maze = new Maze("medium");
+        Maze maze = new Maze("large");
         System.out.println("BFS: " + maze.breadthFirstSearch());
         System.out.println("DFS: " + maze.depthFirstSearch());
     }
